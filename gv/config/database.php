482b24914e438055bc49290a75de43cf4788434c
ricardovargas4 <ricardovargas4@msn.com>
2018-08-07 15:57:35 -0300

    MariaDB

--- gv/config/database.php
+++ gv/config/database.php
@@ -42,7 +42,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
+            'port' => env('DB_PORT', '3307'),
             'database' => env('DB_DATABASE', 'gvdb'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
