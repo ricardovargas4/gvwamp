c2c08bc044940314375fd4931506f2edcc7a31ac
ricardovargas4 <ricardovargas4@msn.com>
2018-08-15 13:37:50 -0300

    Ajuste Data Command Indicador

--- gv/app/Console/Commands/ProcIndicador.php
+++ gv/app/Console/Commands/ProcIndicador.php
@@ -40,7 +40,7 @@ class ProcIndicador extends Command
     public function handle()
     {
         /*Alterar Criacao de Historico por CRUD*/
-        $data = DB::select("SELECT FLOAT_DIAS_UTEIS(NOW(),'-2') data");
+        $data = DB::select("SELECT FLOAT_DIAS_UTEIS(NOW(),'-1') data");
         $data= $data[0]->data;
         $historicos = DB::table('responsavels')
         ->join ('processos','responsavels.id_processo','=','processos.id')
