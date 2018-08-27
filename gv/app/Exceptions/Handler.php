980eb4fb1791ed5faf4c470c4e8914cda85a7392
ricardovargas4 <ricardovargas4@msn.com>
2018-08-27 14:29:21 -0300

    Exceptions Direcionam Home

--- gv/app/Exceptions/Handler.php
+++ gv/app/Exceptions/Handler.php
@@ -44,6 +44,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if (!$exception == null) {
+            return redirect()->guest('home');
+        }
         return parent::render($request, $exception);
     }
 
