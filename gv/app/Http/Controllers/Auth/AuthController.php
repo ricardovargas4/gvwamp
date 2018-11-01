1f27f4a11575c82564ca7a196f4b89d8a8b8db14
Ricardo Vargas <ricardovargas4@msn.com>
2018-11-01 18:09:18 -0300

    LDAP Base DB

--- gv/app/Http/Controllers/Auth/AuthController.php
+++ gv/app/Http/Controllers/Auth/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller {
             return \Redirect::back();
 
         }else{
-            if($usuario->LDAP=="N"){
+            /*if($usuario->LDAP=="N"){
                 $credentials = $request->only('email', 'password');
     
                 if (Auth::attempt($credentials)) {
@@ -29,19 +29,35 @@ class AuthController extends Controller {
                 }else{
                     return \Redirect::back();
                 }
-            }else{
+            }else{*/
                 $ldapusr = $_POST['email'];
-                $ldapini = substr($ldapusr,0,1); 
-                if (empty($ldaprdn)){
+                $basedn="cn=users,dc=sicredi,dc=com,dc=br";
+                $filter='(&(objectClass=inetOrgPerson)(uid='.$ldapusr.'))';  
+                $attributes=array('dn','uid','sn');
+                //$ldapini = substr($ldapusr,0,1); 
+
+                /*if (empty($ldaprdn)){
                     $ldaprdn = 'uid='.$ldapusr.',cn='.$ldapini.',cn=users,dc=sicredi,dc=com,dc=br'; 
                     //dd($ldaprdn);
-                }
+                }*/
                 $ldappass = isset($_POST['password']) ? $_POST['password'] : 'A';
-                $ldapconn = ldap_connect("ldap.sicredi.net")
-                            or die("Could not connect to LDAP server."); 
-                if ($ldapconn) {
+                /*$ldapconn = ldap_connect("ldap.sicredi.net")
+                            or die("Could not connect to LDAP server."); */
+                $cnx = ldap_connect('openldap-slave.sicredi.net',389); // single connection
+                ldap_set_option($cnx, LDAP_OPT_PROTOCOL_VERSION, 3);  
+                
+                $search = ldap_search(array($cnx,$cnx),$basedn,$filter,$attributes);  // search
+                $entries = ldap_get_entries($cnx, $search[0]);  
+        
+                //dd($entries);
+        
+                $dn = $entries[0]['dn'];
+        
+                
+                if ($dn) {
     
-                    $ldapbind = @ldap_bind($ldapconn, $ldaprdn, $ldappass);
+                    //$ldapbind = @ldap_bind($ldapconn, $ldaprdn, $ldappass);
+                    $ldapbind = @ldap_bind($cnx, $dn, $ldappass);
                     if ($ldapbind) {
                                 $expire=time()+60*60;
                                 setcookie("user", $ldapusr, $expire, "/"); 
@@ -52,7 +68,7 @@ class AuthController extends Controller {
                         ->withInput($request->only($this->username())) 
                         ->withErrors($errors);
                     }
-                }
+                //}
     
                 $user = User::where('email','=',$request->email)->first(); 
                 if($user)  {
