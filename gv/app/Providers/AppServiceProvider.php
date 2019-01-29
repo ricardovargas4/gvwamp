88210c543e978ab816e631bfb7bf1dbe1793a16d
Ricardo Vargas <ricardovargas4@msn.com>
2019-01-29 13:38:22 -0200

    Atualzizar Cons Versao

--- gv/app/Providers/AppServiceProvider.php
+++ gv/app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        view()->share('AppVersionCons', '1.0.0.15');
+        view()->share('AppVersionCons', '1.0.0.16');
     }
 
     /**
