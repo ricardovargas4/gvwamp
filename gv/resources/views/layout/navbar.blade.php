c3dc9ad837a9de93e764dd2672de6de6bfc1ed47
Ricardo Vargas <ricardovargas4@msn.com>
2019-03-08 11:28:06 -0300

    Ordenacao NavBar

--- gv/resources/views/layout/navbar.blade.php
+++ gv/resources/views/layout/navbar.blade.php
@@ -16,37 +16,45 @@
             <!-- Dropdown Trigger -->
             <ul id="dropdown1" class="dropdown-content">
               <li><a href="{{ route('atividade.filtro') }}">Atividades</a></li>
-              <li><a href="{{ route('responsavel.lista') }}">Responsaveis</a></li>
-              <li><a href="{{ route('historico.filtro') }}">Histórico Indicadores</a></li>
+              <li><a href="{{ route('demanda.lista') }}">Demandas</a></li>
               <li><a href="{{ route('expurgo.tela') }}">Expurgo Indicador</a></li>
               <li><a href="{{ route('expurgo.filtro') }}">Expurgo Indicador Lista</a></li>
-              <li><a href="{{ route('demanda.lista') }}">Demandas</a></li>
+              <li><a href="{{ route('historico.filtro') }}">Histórico Indicadores</a></li>
+              <li><a href="{{ route('responsavel.lista') }}">Responsaveis</a></li>
               @can('checkGestor')
-                <li><a href="{{ route('processo.lista') }}">Processos</a></li>
-                <li><a href="{{ route('tipo.lista') }}">Tipos</a></li>
-                <li><a href="{{ route('periodicidade.lista') }}">Periodicidades</a></li>
-                <li><a href="{{ route('coordenacao.lista') }}">Coordenações</a></li>
-                <li><a href="{{ route('classificacao.lista') }}">Classificações</a></li>
-                <li><a href="{{ route('usuario.lista') }}">Usuários</a></li>
-                <li><a href="{{ route('conclusao.filtro') }}">Conclusões</a></li>
+                
               @endcan
               <!--<li class="divider"></li>-->
             </ul>
             <li><a class='dropdown-button navBarMenu' href='#' data-activates='dropdown2'>Gráficos<i class="material-icons right">arrow_drop_down</i></a></li>
               <ul id="dropdown2" class="dropdown-content">
-                <li><a href="{{ route('home.tempo') }}">Tempos</a></li>
                 <li><a href="{{ route('home.indicador') }}">Indicador</a></li>
+                <li><a href="{{ route('home.tempo') }}">Tempos</a></li>
               </ul>
-            @can('checkGestor')
+              @can('checkGestor')
               <li><a class='dropdown-button navBarMenu' href='#' data-activates='dropdown3'>Relatórios<i class="material-icons right">arrow_drop_down</i></a></li>
               <ul id="dropdown3" class="dropdown-content">
                 <li><a href="{{ route('resp.RelatorioResp') }}">Download Responsáveis</a></li>
                 <li><a href="{{ route('indic.RelatorioIndicador') }}">Relatórios Indicador</a></li>
                 <li><a href="{{ route('atividade.RelatorioTempos') }}">Relatórios Tempos</a></li>
               </ul>
+              @endcan  
+              <!-- Dropdown Trigger -->
+              <li><a class='dropdown-button navBarMenu' href='#' data-activates='dropdown1'>Manutenção<i class="material-icons right">arrow_drop_down</i></a></li>
+              <!--Divisão -->
+            @can('checkGestor')
+              <li><a class='dropdown-button navBarMenu' href='#' data-activates='dropdown4'>Manutenção Gestor<i class="material-icons right">arrow_drop_down</i></a></li>
+              <ul id="dropdown4" class="dropdown-content">
+                <li><a href="{{ route('classificacao.lista') }}">Classificações</a></li>
+                <li><a href="{{ route('coordenacao.lista') }}">Coordenações</a></li>
+                <li><a href="{{ route('conclusao.filtro') }}">Conclusões</a></li>
+                <li><a href="{{ route('periodicidade.lista') }}">Periodicidades</a></li>
+                <li><a href="{{ route('processo.lista') }}">Processos</a></li>
+                <li><a href="{{ route('tipo.lista') }}">Tipos</a></li>
+                <li><a href="{{ route('usuario.lista') }}">Usuários</a></li>
+              </ul>
             @endcan  
-            <!-- Dropdown Trigger -->
-            <li><a class='dropdown-button navBarMenu' href='#' data-activates='dropdown1'>Manutenção<i class="material-icons right">arrow_drop_down</i></a></li>
+              <!--Fim Divisão -->
           </ul>
         </div>
       </div>
